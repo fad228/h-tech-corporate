6b6dac608d4beb1e9fd30ec13c31faa82c88ac3d
fad228 <fadilekpaye@gmail.com>
2025-05-03 22:19:15 +0000

    Ajout du sitemap et mise à jour de robots.txt

--- htech_corporate/urls.py
+++ htech_corporate/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from main.sitemaps import StaticViewSitemap
+from django.views.generic import TemplateView
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -30,4 +31,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
